d8b2b5dd228c14de45163dfc0cf68290b144e133
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-08-17 01:21:57 +0500

    first commit

--- bufoi/Os.go
+++ bufoi/Os.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 
@@ -18,10 +20,35 @@ func main() {
 	// age,_:=rander.ReadString('\n');
 	// fmt.Printf("Your age is and %T %v",age)
 
-        fmt.Print("HI my name is Abdullah.Hoe are you");
+
+// ---------------------------------------------------------------------------
+    
+
+     // fmt.Print("HI my name is Abdullah.Hoe are you");
+		// fmt.Print("\n");
+		// render :=bufio.NewReader(os.Stdin);
+		// fmt.Printf("Enter your age ");
+		// name,_:=render.ReadString('\n')
+		// fmt.Printf("Your name is %s ,%T",name,name)
+
+		 fmt.Print("HI my name is Abdullah.Hoe are you");
 		fmt.Print("\n");
 		render :=bufio.NewReader(os.Stdin);
 		fmt.Printf("Enter your age ");
 		name,_:=render.ReadString('\n')
-		fmt.Printf("Your name is %s ,%T",name,name)
+		// fmt.Printf("Your name is %s ,%T",name,name)
+       
+		change,err :=strconv.ParseFloat(strings.TrimSpace(name),64)
+		if err!=nil{
+            fmt.Println("Error occurred while converting string to float")
+        }else{
+            fmt.Printf("Converted age is%v",change+1,)
+            fmt.Printf("%T",change)
+			
+			
+
+        }
+
+
+
 }
